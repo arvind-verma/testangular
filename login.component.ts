83d050e77c6d207137cbbda5113977914013e803
arvindverma <21arvind.verma@gmail.com>
2022-04-05 22:20:31 +0530

    Changes test form

---
+++ login.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, TemplateRef, Renderer2, EventEmitter,ViewChild,ElementRef } from '@angular/core';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+
+    full_name_test;
+    email_test
+    grid_row:any = [];
+    saveData(){
+      let grid = {
+        "name":this.full_name_test,
+        "email":this.email_test
+      } 
+      this.grid_row.push(grid);
+    }
+
+    editAction ="-1";
+    edit(id){
+      this.editAction = id;
+      if(typeof this.grid_row[id]!="undefined"){
+        this.full_name_test = this.grid_row[id].name;
+        this.email_test = this.grid_row[id].email;
+      }
+    }
+}
